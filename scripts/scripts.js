97caf82e55917af79e5208631c3384d7fe938a5d
ruby <rubycao04@gmail.com>
2022-11-08 10:22:37 -0800

    do while loops

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,7 +7,22 @@ for (let i = 2; i < 11; i++) {
     console.log(2 ** i);
 }
 
-let i = 1 
-while (i < 11) {
-    console.log(2 ** i)
+// let i = 1 
+// while (i < 11) {
+//     console.log(Math.pow(2, i));
+// }
+
+let input = ""
+let numInput = ""
+do {
+    input = prompt( "what is your name");
+    //prompt always returns a string
+    //we need to convert the string to a number and check if it was successfully converte
+    //nan when user enters a non numerical string
 }
+
+   
+while (!is Nan(Number(input)));
+console.log(input);
+
+while (Number(input)) || input == 0
